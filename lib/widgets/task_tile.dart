7bfd4c7548adee6de8d8b26a9c62454c00ddca84
wpinrui <77185900+wpinrui@users.noreply.github.com>
2023-06-08 18:10:53 +0800

    Update checkbox state

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     super.key,
   });
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxCallback(bool? currentState) {
+    setState(() {
+      isChecked = currentState!;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +29,29 @@ class TaskTile extends StatelessWidget {
               'This is an easy task.',
               style: TextStyle(
                 fontSize: 20,
+                decoration: isChecked ? TextDecoration.lineThrough : null,
               ),
             ),
           ),
         ),
-        Checkbox(value: false, onChanged: (value) {})
+        TaskCheckbox(isChecked: isChecked, callback: checkboxCallback),
       ],
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool isChecked;
+  final void Function(bool?) callback;
+  const TaskCheckbox(
+      {super.key, required this.isChecked, required this.callback});
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: (value) {
+        callback(value);
+      },
     );
   }
 }
